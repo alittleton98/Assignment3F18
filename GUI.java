75768aa873a795cd3ed16a3557aec39fcfa856fd
Kyle Lieu <klieu1@luc.edu>
2018-11-07 11:30:29 -0600

    Added object labels

--- GUI.java
+++ GUI.java
@@ -1,32 +1,31 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 
-public class GUI extends JFrame{
 
-    JPanel p=new JPanel();
-    JLabel b=new JLabel("Please Paste a Reddit URL Below");
-    JTextField t=new JTextField("",45);
+class GUI extends JFrame{
+    JPanel panel=new JPanel();
+    JLabel instruction=new JLabel("Please Paste a Reddit URL Below");
+    JTextField textEntry=new JTextField("",45);
 
-    JLabel l=new JLabel("Choose a save location");
+    JLabel saveLocation=new JLabel("Save location:");
     String choices[] = {
             "Downloads",
             "Desktop",
             "Documents",
             "Pictures",
-            "Root"
+            "Root",
     };
-    JComboBox cb = new JComboBox(choices);
 
-    JButton r=new JButton("Begin Image Scrape");
-    JButton exitButton = new JButton("Exit");
+    JComboBox choicesButton = new JComboBox(choices);
+    JButton beginButton=new JButton("Begin Image Scrape");
     JButton restartButton = new JButton("Clear");
-    
+    JButton exit = new JButton("Exit");
+
     public static void main(String[] args){
         new GUI();
     }
@@ -35,14 +34,14 @@ public class GUI extends JFrame{
         super("Reddit Image Scraper");
         setSize(625,600);
         setResizable(true);
-        p.add(b);
-        p.add(t);
-        p.add(l);
-        p.add(cb);
-        p.add(r);
-        p.add(restartButton);
-        p.add(exitButton);
-        add(p);
+        panel.add(instruction);
+        panel.add(textEntry);
+        panel.add(saveLocation);
+        panel.add(choicesButton);
+        panel.add(beginButton);
+        panel.add(restartButton);
+        panel.add(exit);
+        add(panel);
         setVisible(true);
     }
 }
